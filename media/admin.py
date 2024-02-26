a00786c61f6e82b9b860c151b84551d5a20daf57
pathangufran <gufranpathann@gmail.com>
2024-02-26 14:22:56 +0530

    super user added

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from media.models import UserProfile,Post,Like,Connection
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Connection)
+admin.site.register(Post)
+admin.site.register(Like)
